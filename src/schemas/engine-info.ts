832d189015cdcf1bf87a380db93127ee6d6bc223
NonSpicyBurrito <burritoyangster@gmail.com>
2022-04-23 18:06:21 +0800

    Updated engine version

--- src/schemas/engine-info.ts
+++ src/schemas/engine-info.ts
@@ -3,7 +3,7 @@ import { localizationTextSchema } from './localization-text'
 import { getParser } from './parser'
 
 const partialEngineInfoSchema = z.object({
-    version: z.literal(4),
+    version: z.literal(5),
     title: localizationTextSchema,
     subtitle: localizationTextSchema,
     author: localizationTextSchema,
