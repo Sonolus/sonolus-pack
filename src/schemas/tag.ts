29ccad651b084d28a7c1d18287defcc1b0b5c6ef
Burrito <burritoyangster@gmail.com>
2024-06-16 16:22:50 +0800

    Updated to support Sonolus 0.8.3

--- src/schemas/tag.ts
+++ src/schemas/tag.ts
@@ -1,12 +1,12 @@
 import { Type } from '@sinclair/typebox'
-import { DatabaseTag, Icon } from '@sonolus/core'
+import { DatabaseTag } from '@sonolus/core'
 import { Expect } from '../utils/test'
 import { localizationTextSchema } from './localizationText'
 import { SchemaToMatch } from './test'
 
 export const databaseTagSchema = Type.Object({
     title: localizationTextSchema,
-    icon: Type.Optional(Type.Union(Object.values(Icon).map((icon) => Type.Literal(icon)))),
+    icon: Type.Optional(Type.String()),
 })
 
 type _Tests = Expect<[SchemaToMatch<typeof databaseTagSchema, DatabaseTag>]>
