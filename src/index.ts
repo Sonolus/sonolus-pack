1af9caaa927e5e7fcf4ba9c3b47e8011254c2832
Burrito <burritoyangster@gmail.com>
2025-02-06 20:48:07 +0800

    Release 5.4.7 (#26)

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import { partialDatabaseServerInfoSchema } from './schemas/serverInfo'
 
 const options = new Command()
     .name('sonolus-pack')
-    .version('5.4.6')
+    .version('5.4.7')
     .option('-i, --input <value>', 'input directory', 'source')
     .option('-o, --output <value>', 'output directory', 'pack')
     .parse()
